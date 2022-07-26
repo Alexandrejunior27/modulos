6844d9bd2f435803f81293ecc72ecae4876443e7
Alexandre <alexandrejahneljunior@gmail.com>
2022-07-26 16:28:31 -0300

    Trabalhando com modulos em JavaScript

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Alexandre", 19));
+console.log(mostraCidade("ALexandre", "São Paulo"));
+console.log(mostraHobby("Alexandre", "treinar"));
